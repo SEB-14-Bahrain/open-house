2156b05304204a0cb69ed2a4599db29230cc3771
nayaba <nabila.ayaba@gmail.com>
2026-07-15 11:30:34 +0300

    Creates Listing model

---
+++ models/listing.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const listingSchema = new mongoose.Schema({
+    price: {
+        type: Number,
+        required: true,
+        min: 0, 
+    },
+    image: {
+        type: String,
+        default: 'https://images.pexels.com/photos/1438832/pexels-photo-1438832.jpeg',
+    },
+    streetAddress: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    size: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+}, { timestamps: true } )
+
+const Listing = mongoose.model('Listing', listingSchema)
+
+module.exports = Listing
